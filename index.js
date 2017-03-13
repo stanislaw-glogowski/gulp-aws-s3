eac3df9707994c1797bbc8dc5450e27ab58bab89
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2017-03-13 10:36:44 +0100

    Node version update, switch to es2016

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var _     = require('lodash'),
-    es    = require('event-stream'),
-    AWS   = require('aws-sdk'),
-    mime  = require('mime'),
-    gutil = require('gulp-util');
+const _ = require('lodash');
+const es = require('event-stream');
+const AWS = require('aws-sdk');
+const mime = require('mime');
+const gutil = require('gulp-util');
 
 /**
  * plugin name
@@ -10,113 +10,110 @@ var _     = require('lodash'),
  */
 const PLUGIN_NAME = 'gulp-aws-s3';
 
-var plugin = {
-    config : {},
+const plugin = {
+  config: {},
 
-    /**
-     * default config values
-     */
-    defaults : {
-        key    : process.env.AWS_ACCESS_KEY_ID || null,
-        secret : process.env.AWS_SECRET_ACCESS_KEY || null,
-        region : process.env.AWS_REGION || null,
-        bucket : process.env.AWS_BUCKET || null
-    },
+  /**
+   * default config values
+   */
+  defaults: {
+    key: process.env.AWS_ACCESS_KEY_ID || null,
+    secret: process.env.AWS_SECRET_ACCESS_KEY || null,
+    region: process.env.AWS_REGION || null,
+    bucket: process.env.AWS_BUCKET || null,
+  },
 
-    /**
-     * config setup
-     * @param config
-     * @returns {plugin}
-     */
-    setup : function(config) {
-        this.config = _.defaults({}, config, this.config, this.defaults);
-        return this;
-    },
+  /**
+   * config setup
+   * @param config
+   * @returns {plugin}
+   */
+  setup(config) {
+    this.config = _.defaults({}, config, this.config, this.defaults);
+    return this;
+  },
 
-    /**
-     * upload sub plugin
-     * @returns {*}
-     */
-    upload : function() {
-        var options = arguments[0] || {},
-            config  = arguments[1] || {};
+  /**
+   * upload sub plugin
+   * @returns {*}
+   */
+  upload(opts = { acl: 'public-read', path: '' }, cfg = {}) {
+    const options = opts;
+    const config = cfg;
 
-        _.each(this.config, function(value, key) {
-            config[key] = config[key] ? config[key] : value;
-        });
+    _.each(this.config, (value, key) => {
+      config[key] = config[key] ? config[key] : value;
+    });
 
-        _.defaults(options, {
-            acl  : 'public-read',
-            path : ''
-        });
-
-        if (!config.key) {
-            throw new Error('Missing config `key`');
-        }
-        if (!config.secret) {
-            throw new Error('Missing config `secret`');
-        }
-        if (!config.region) {
-            throw new Error('Missing config `region`');
-        }
-        if (!config.bucket) {
-            throw new Error('Missing config `bucket`');
-        }
-
-        return es.map(function(file, callback) {
-            if (!file.isBuffer()) {
-                return false;
-            }
-
-            var s3 = new AWS.S3({
-                accessKeyId     : config.key,
-                secretAccessKey : config.secret,
-                region          : config.region
-            });
-
-            if (file.path) {
-                options.key = file.path.replace(file.base, options.path || '');
-                options.key = options.key.replace(new RegExp('\\\\', 'g'), '/');
-            }
-
-            if (!options.key) {
-                throw new Error('Missing option `key`');
-            }
-
-            var keyParts  = options.key.split('.'),
-                extension = keyParts.length > 1 ? keyParts[keyParts.length - 1] : null,
-                params    = {
-                    Bucket : config.bucket,
-                    Key    : options.key,
-                    ACL    : options.acl,
-                    Body   : file.contents
-                };
-
-            if (extension) {
-                params['ContentType'] = mime.lookup(file.path);
-
-                _.each(options.params || {}, function(value, key) {
-                    params[key] = value;
-
-                })
-
-                if (file.stat) {
-                    params['ContentLength'] = file.stat.size;
-                }
-            }
-
-            s3.putObject(params, function(err) {
-                if (err) {
-                    gutil.log('S3::putObject', "'" + gutil.colors.red(params.Key) + "'", gutil.colors.red('error!'));
-                    throw err;
-                } else {
-                    gutil.log('S3::putObject', "'" + gutil.colors.cyan(params.Key) + "'", 'sent.');
-                    gutil.beep();
-                }
-                callback(err);
-            });
-        });
+    if (!config.key) {
+      throw new Error('Missing config `key`');
     }
+    if (!config.secret) {
+      throw new Error('Missing config `secret`');
+    }
+    if (!config.region) {
+      throw new Error('Missing config `region`');
+    }
+    if (!config.bucket) {
+      throw new Error('Missing config `bucket`');
+    }
+
+    return es.map((file, callback) => {
+      if (!file.isBuffer()) {
+        return false;
+      }
+
+      const s3 = new AWS.S3({
+        accessKeyId: config.key,
+        secretAccessKey: config.secret,
+        region: config.region,
+      });
+
+      if (file.path) {
+        options.key = file.path.replace(file.base, options.path || '');
+        options.key = options.key.replace(new RegExp('\\\\', 'g'), '/');
+      }
+
+      if (!options.key) {
+        throw new Error('Missing option `key`');
+      }
+
+      const keyParts = options.key.split('.');
+      const extension = keyParts.length > 1 ? keyParts[keyParts.length - 1] : null;
+      const params = {
+        Bucket: config.bucket,
+        Key: options.key,
+        ACL: options.acl,
+        Body: file.contents,
+      };
+
+      if (extension) {
+        params.ContentType = mime.lookup(file.path);
+
+        _.each(options.params || {}, (value, key) => params[key] = value)
+
+        if (file.stat) {
+          params.ContentLength = file.stat.size;
+        }
+      }
+
+      s3.putObject(params, (err) => {
+        const key = `'${gutil.colors.red(params.Key)}`;
+
+        if (err) {
+          gutil.log('S3::putObject', key, gutil.colors.red('error!'));
+          throw err;
+        }
+
+        gutil.log('S3::putObject', key, 'sent.');
+        gutil.beep();
+
+        callback(err);
+      });
+
+      return null;
+    });
+  },
 };
 
 /**
--- test/upload.js
+++ test/upload.js
@@ -1,20 +1,14 @@
-var assert      = require('assert'),
-    es          = require('event-stream'),
-    gutil       = require('gulp-util'),
-    awsS3       = require('../index');
+const gutil = require('gulp-util');
+const awsS3 = require('../index');
 
-describe('upload', function() {
+describe('upload', () => {
+  it('should create fake file', (done) => {
+    const stream = awsS3.upload({ key: 'fake' });
+    const fakeBuffer = new Buffer('fake');
+    const fakeFile = new gutil.File({ contents: fakeBuffer });
 
-    it('should create fake file', function(done) {
-        var stream     = awsS3.upload({ key : 'fake' }),
-            fakeBuffer = new Buffer('fake'),
-            fakeFile   = new gutil.File({ contents: fakeBuffer });
-
-        stream.on('end', function() {
-            done();
-        });
-
-        stream.write(fakeFile);
-        stream.end();
-    });
+    stream.on('end', () => done());
+    stream.write(fakeFile);
+    stream.end();
+  });
 });
